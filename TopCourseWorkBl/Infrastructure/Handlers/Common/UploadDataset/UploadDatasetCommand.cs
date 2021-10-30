fd95dcdf2a1244dfda5f728c4118234c13fa9f0d
daniilda <daniilda@hotmail.com>
2021-10-30 21:31:45 +0300

    [DEV-1] Resolves issue DEV-1 (#4)
    * [DEV-1] updating files + new libs
    
    * [DEV-1] add common controller + handle for uploading datasets
    
    Co-authored-by: Kuznetsov Daniil Andreevich <danikuznetsov@ozon.ru>

---
+++ TopCourseWorkBl/Infrastructure/Handlers/Common/UploadDataset/UploadDatasetCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TopCourseWorkBl.Infrastructure.Handlers.Common.UploadDataset
+{
+    public record UploadDatasetCommand(IFormFileCollection Files) : IRequest<IActionResult>;
+}
